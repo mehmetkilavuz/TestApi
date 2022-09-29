744491169ca6638536ee5125c60efa2c4c6e36a7
mehmetkilavuz <47543160+mehmetkilavuz@users.noreply.github.com>
2022-09-30 00:48:51 +0300

    MSK:docker file

--- TestApiLast/Program.cs
+++ TestApiLast/Program.cs
@@ -6,7 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+ 
 
 var app = builder.Build();
 
@@ -16,7 +16,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
